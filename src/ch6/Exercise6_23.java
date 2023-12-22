dd605f0d84e16f39bed55c8374acd96fe54c8e90
unknown <hakie2kim@gmail.com>
2023-12-22 13:42:44 +0900

    PrimitiveParamEx.java, ReferenceParamEx.java, ReferenceReturnEx.java

--- src/ch6/Exercise6_23.java
+++ src/ch6/Exercise6_23.java
@@ -1,7 +1,6 @@
 package ch6;
 
 public class Exercise6_23 {
-
     public static void main(String[] args) {
         int[] data = {3, 2, 9, 4, 7};
         System.out.println(java.util.Arrays.toString(data));
--- src/ch6/Exercise6_24.java
+++ src/ch6/Exercise6_24.java
@@ -1,25 +1,14 @@
 package ch6;
 
-public class Exercise6_23 {
-
+public class Exercise6_24 {
     public static void main(String[] args) {
-        int[] data = {3, 2, 9, 4, 7};
-        System.out.println(java.util.Arrays.toString(data));
-        System.out.println("최대값:" + max(data));
-        System.out.println("최대값:" + max(null));
-        System.out.println("최대값:" + max(new int[]{})); // 크기가 0인 배열
+        int value = 5;
+        System.out.println(value + "의 절대값:" + abs(value));
+        value = -10;
+        System.out.println(value + "의 절대값:" + abs(value));
     }
 
-    private static int max(int[] arr) {
-        if (arr == null || arr.length == 0)
-            return -999999;
-
-        int max = arr[0];
-        // 배열을 돌면서 기존 설정한 값보다 큰 값을 갖고 있는 요소가 나오면 max 값을 바꿔준다.
-        for (int i = 0; i < arr.length; i++) {
-            if (max < arr[i])
-                max = arr[i];
-        }
-        return max;
+    private static int abs(int value) {
+        return value > 0 ? value : value * -1;
     }
 }
